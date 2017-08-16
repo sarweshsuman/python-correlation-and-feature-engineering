e18aa7afec32c513538c5ef7c9d6b40f11981531
sarweshsuman <sarweshsuman.1@gmail.com>
2017-08-16 15:11:57 +0530

    Update pearsonr_correlation_with_pandas.py

--- pearsonr_correlation_with_pandas.py
+++ pearsonr_correlation_with_pandas.py
@@ -1,2 +1,10 @@
 import pandas as pd
 
+from python_utilities.read_input_pandas import read_csv
+import sys
+
+file_name = sys.argv[1]
+
+data_frame = read_csv(file_name,',')
+
+print(data_frame.corr())  # will give the correlation between each data point vs other data point in a tabular format.
